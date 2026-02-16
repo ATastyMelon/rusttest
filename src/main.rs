ff7b3843deb1f044431142607febdf152929c859
Melon <g.redding2007@gmail.com>
2026-02-15 17:57:21 -0800

    Basic rp2040 bones in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+use rp2040_hal as hal;
+
+use hal::entry;
+use cortex_m_semihosting::hprintln;
+
+#[unsafe(link_section = ".boot2")]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+#[entry]
+fn _start() -> ! {
+    hprintln!("Hello World!");
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
