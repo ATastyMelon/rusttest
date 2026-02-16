9c3068718cf28984470049b51b6392d87e3072a2
Melon <g.redding2007@gmail.com>
2026-02-15 20:46:48 -0800

    it works now

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use hal::entry;
 
 use rp2040_hal as hal;
 
-
 #[unsafe(link_section = ".boot2")]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
@@ -21,8 +20,9 @@ const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 fn _start() -> ! {
     let mut pac = hal::pac::Peripherals::take().unwrap();
 
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+        let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
+    // Configure the clocks
     let clocks = hal::clocks::init_clocks_and_plls(
         XTAL_FREQ_HZ,
         pac.XOSC,
@@ -32,20 +32,19 @@ fn _start() -> ! {
         &mut pac.RESETS,
         &mut watchdog,
     )
-    .ok()
     .unwrap();
 
     let mut timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
     let sio = hal::Sio::new(pac.SIO);
 
-    let pins = hal::gpio::Pins::new(
+        let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-
+    
     let mut led_pin = pins.gpio25.into_push_pull_output();
 
     led_pin.set_high().unwrap();
