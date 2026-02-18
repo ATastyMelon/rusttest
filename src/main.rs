faf26887e50c9ebd0adf85aea9c0eed0174a14f4
Melon <g.redding2007@gmail.com>
2026-02-18 14:50:12 -0800

    Tried embassy, not sure if i like it or not, so i switched back

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,10 @@ use core::panic::PanicInfo;
 
 use cortex_m_semihosting::hprintln;
 
-use embedded_hal::delay::DelayNs;
 use embedded_hal::digital::OutputPin;
-
 use hal::entry;
 
-use rp2040_hal as hal;
-
-unsafe extern "C" {
-    static __PAYLOAD: u32;
-    static __PAYLOAD_END: u32;
-}
+use rp2040_hal::{self as hal, pac::watchdog};
 
 #[unsafe(link_section = ".boot2")]
 #[used]
@@ -26,7 +19,6 @@ const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 #[entry]
 fn main() -> ! {
     let mut pac = hal::pac::Peripherals::take().unwrap();
-
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
     let clocks = hal::clocks::init_clocks_and_plls(
@@ -36,32 +28,26 @@ fn main() -> ! {
         pac.PLL_SYS,
         pac.PLL_USB,
         &mut pac.RESETS,
-        &mut watchdog,
+        &mut watchdog
     )
     .ok()
     .unwrap();
 
-    let sio = hal::Sio::new(pac.SIO);
+    let mut sio = hal::Sio::new(pac.SIO);
 
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
-        &mut pac.RESETS,
+        &mut pac.RESETS   
     );
 
-    let mut timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
-    
     let mut led_pin = pins.gpio25.into_push_pull_output();
 
-    loop {
-        led_pin.set_high().unwrap();
-        timer.delay_ms(500);
-        led_pin.set_low().unwrap();
-        timer.delay_ms(500);
+    led_pin.set_high().unwrap();
 
-        watchdog.feed();
-    }
+    loop {}
+    
 }
 
 #[panic_handler]
