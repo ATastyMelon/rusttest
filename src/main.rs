ebdb815a57f1f2d9df36fe4aca755dd063cd923c
Melon <g.redding2007@gmail.com>
2026-02-19 20:15:19 -0800

    Nevermind, embassy is cool

--- src/main.rs
+++ src/main.rs
@@ -3,55 +3,29 @@
 
 use core::panic::PanicInfo;
 
-use cortex_m_semihosting::hprintln;
+use embassy_executor::Spawner;
+use embassy_rp::{self as hal, gpio::{Level, Output}, watchdog::Watchdog};
+use embassy_time::Timer;
 
-use embedded_hal::digital::OutputPin;
-use hal::entry;
+use defmt_rtt as _;
 
-use rp2040_hal::{self as hal, pac::watchdog};
+#[allow(unused)]
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = hal::init(Default::default());
+    let mut watchdog = Watchdog::new(p.WATCHDOG);
+    let mut led_pin = Output::new(p.PIN_25, Level::Low);
 
-#[unsafe(link_section = ".boot2")]
-#[used]
-pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+    led_pin.set_high();
 
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
-
-#[entry]
-fn main() -> ! {
-    let mut pac = hal::pac::Peripherals::take().unwrap();
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    let clocks = hal::clocks::init_clocks_and_plls(
-        XTAL_FREQ_HZ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog
-    )
-    .ok()
-    .unwrap();
-
-    let mut sio = hal::Sio::new(pac.SIO);
-
-    let pins = hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS   
-    );
-
-    let mut led_pin = pins.gpio25.into_push_pull_output();
-
-    led_pin.set_high().unwrap();
-
-    loop {}
+    loop {
+        Timer::after_millis(15).await;
+        watchdog.feed();
+    }
     
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    hprintln!("{}", _info);
     loop {}
 }
